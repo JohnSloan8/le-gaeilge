1a65102299353b40fc9ff3a3d0ed269126ccb033
JohnSloan8 <johnsloan1984@gmail.com>
2024-04-18 16:56:41 +0100

    removing quotes

--- components/cards/GroupCardSmall.tsx
+++ components/cards/GroupCardSmall.tsx
@@ -12,7 +12,7 @@ export default function GroupCardSmall({
   image,
 }: GroupCardSmallProps) {
   return (
-    <div className="w-[160px] p-1 md:p-3 flex flex-col rounded-md items-center h-[200px] border-2 bg-purple-50 border-purple-300 hover:border-purple-400 hover:bg-purple-50">
+    <div className="w-[160px] p-1 md:p-2 flex flex-col rounded-md items-center h-[200px] border-2 bg-purple-50 border-purple-300 hover:border-purple-400 hover:bg-purple-50">
       <GroupImage url={image} />
       <SmallTitle text_ga={""} text_en={text_en} centered={true} />
     </div>
